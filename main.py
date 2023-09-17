2289714434d4508649e4cc5b74cd84d42bfe193d
JoannaSos <joanna.arendarska@gmail.com>
2023-09-17 17:39:38 +0200

    Add tests with dates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import datetime
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -6,6 +7,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+
+from utils import months
 
 
 class SearchingTests(unittest.TestCase):
@@ -32,7 +35,6 @@ class SearchingTests(unittest.TestCase):
         self.findProwlyInGoogle()
         search_results_by_all = self.driver.find_elements(By.CLASS_NAME, "MjjYud")
         is_prowly_present = all("prowly" in result.text.lower() for result in search_results_by_all)
-        print(is_prowly_present)
         self.assertTrue(is_prowly_present, "The word 'prowly' is not present in the search results")
 
     # test searching by video
@@ -48,14 +50,56 @@ class SearchingTests(unittest.TestCase):
         self.assertTrue(is_prowly_present, "The word 'prowly' is not present in the search results")
 
     # test searching by news
-    def testSearchingNews(self):
+    def testSearchingCareers(self):
         self.findProwlyInGoogle()
-        button_videos = self.driver.find_element(By.XPATH, "//span[contains(text(), 'Wiadomości')]")
-        button_videos.click()
+        button_careers = WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'Careers')]")))
+        button_careers.click()
         WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.ID, "result-stats"))
         )
-        search_result_by_video = self.driver.find_elements(By.CLASS_NAME, "SoaBEf")
-        is_prowly_present = all("prowly" in result.text.lower() for result in search_result_by_video)
+        search_result_by_careers = self.driver.find_elements(By.CLASS_NAME, "SoaBEf")
+        is_prowly_present = all("prowly" in result.text.lower() for result in search_result_by_careers)
         self.assertTrue(is_prowly_present, "The word 'prowly' is not present in the search results")
 
+        # method searching by date
+
+    def changeDateFilter(self, text):
+        self.findProwlyInGoogle()
+        button_tool = WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Narzędzia')]")))
+
+        button_tool.click()
+
+        button_ever = WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Kiedykolwiek')]")))
+
+        button_ever.click()
+        xpath = "//a[contains(text(),'" + text + "')]"
+        button_time_interval = self.driver.find_element(By.XPATH, xpath)
+        button_time_interval.click()
+
+    def testAllByLastWeek(self):
+        self.filteringByDate('Ostatni tydzień', 7)
+
+    def testAllByLastMonth(self):
+        self.filteringByDate('Ostatni miesiąc', 31)
+
+    def testAllByLastYear(self):
+        self.filteringByDate('Ostatni rok', 366)
+
+    def filteringByDate(self, date_filter_name, days):
+        self.changeDateFilter(date_filter_name)
+        search_results = self.driver.find_elements(By.CSS_SELECTOR, "div.g")
+        for i in search_results:
+            date_span = i.find_element(By.CLASS_NAME, "MUxGbd.wuQ4Ob.WZ8Tjf")
+            if date_span is not None:
+                date_list = date_span.text.split(" ")
+                if (date_list[1] == "dni" or date_list[1] == "dzień") and date_list[2] == "temu":
+                    self.assertLessEqual(int(date_list[0]), days)
+                else:
+                    month = months[date_list[1]]
+                    date = datetime(int(date_list[2]), month, int(date_list[0]))
+                    today = datetime.now()
+                    x = today - date
+                    self.assertLessEqual(x.days, days)
