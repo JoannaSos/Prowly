790b73d5b3b9425424c2c8ccf66380658f027d81
JoannaSos <joanna.arendarska@gmail.com>
2023-09-17 16:31:13 +0200

    Add test for searching if videos contain text "prowly"

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import unittest
+
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class SearchingTests(unittest.TestCase):
@@ -14,7 +15,6 @@ class SearchingTests(unittest.TestCase):
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.maximize_window()
         self.driver.get("https://www.google.pl/?hl=pl")
-
 
     # searching method
     def findProwlyInGoogle(self):
@@ -33,4 +33,16 @@ class SearchingTests(unittest.TestCase):
         search_results_by_all = self.driver.find_elements(By.CLASS_NAME, "MjjYud")
         is_prowly_present = all("prowly" in result.text.lower() for result in search_results_by_all)
         print(is_prowly_present)
+        self.assertTrue(is_prowly_present, "The word 'prowly' is not present in the search results")
+
+    # test searching by video
+    def testSearchingVideos(self):
+        self.findProwlyInGoogle()
+        button_videos = self.driver.find_element(By.XPATH, "//span[contains(text(), 'Wideo')]")
+        button_videos.click()
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.ID, "result-stats"))
+        )
+        search_result_by_video = self.driver.find_elements(By.CLASS_NAME, "g")
+        is_prowly_present = all("prowly" in result.text.lower() for result in search_result_by_video)
         self.assertTrue(is_prowly_present, "The word 'prowly' is not present in the search results")
